e2e4c0b1865f28b2dea9314b0fd4703d67f819d2
fathurid <fathuriz.id@gmail.com>
2022-05-24 10:01:57 +0700

    menambahkan file bootstrap CRUD

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -13,96 +13,92 @@
 
                         <!-- Earnings (Monthly) Card Example -->
                         <div class="col-xl-3 col-md-6 mb-4">
-                            <div class="card border-left-primary shadow h-100 py-2">
-                                <div class="card-body">
-                                    <div class="row no-gutters align-items-center">
-                                        <div class="col mr-2">
-                                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                                Jumlah Anggota</div>
-                                                <div class="h1 mb-0 font-weight-bold text-black"><?=
-                                                $this->ModelUser->getUserWhere(['role_id' => 1])->num_rows();?>
-                                            </div>
+                        <div class="card border-left-danger shadow h-100 py-2 bg-primary">
+                            <div class="card-body">
+                                <div class="row no-gutters align-items-center">
+                                    <div class="col mr-2">
+                                        <div class="text-md font-weight-bold text-white text-uppercase mb-1">Jumlah Anggota</div>
+                                        <div class="h1 mb-0 font-weight-bold text-white"><?=
+                                        $this->ModelUser->getUserWhere(['role_id' => 1])->num_rows(); ?>
                                         </div>
-                                        <div class="col-auto">
-                                        <a href="<?= base_url('user/anggota'); ?>">
-                                        <i class="fas fa-users fa-3x text-warning"></i></a>
-                                        <i class="fas fa-calendar fa-2x text-gray-300"></i>
+                                    </div>
+                                    <div class="col-auto">
+                                        <a href="<?= base_url('user/anggota'); ?>"><i
+                                        class="fas fa-users fa-3x text-warning"></i></a>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
 
+
                         <!-- Earnings (Monthly) Card Example -->
                         <div class="col-xl-3 col-md-6 mb-4">
-                            <div class="card border-left-success shadow h-100 py-2">
+                            <div class="card border-left-primary shadow h-100 py-2 bg-warning">
                                 <div class="card-body">
                                     <div class="row no-gutters align-items-center">
                                         <div class="col mr-2">
-                                            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
-                                                Stock Buku Terdaftar</div>
-                                                <div class="h1 mb-0 font-weight-bold text-black">
-                                                    <?php
-                                                    $where = ['stok != 0'];
-                                                    $totalstok = $this->ModelBuku->total('stok', $where);
-                                                    echo $totalstok; ?>
+                                            <div class="text-md font-weight-bold text-white text-uppercase mb-1">Stok Buku Terdaftar</div>
+                                            <div class="h1 mb-0 font-weight-bold text-white">
+                                                <?php
+                                                $where = ['stok != 0'];
+                                                $totalstok = $this->ModelBuku->total('stok', $where);
+                                                echo $totalstok;?>
                                                 </div>
+                                            </div>
+                                            <div class="col-auto">
+                                                <a href="<?= base_url('buku'); ?>"><i class="fas fa-book fa-3x text-primary"></i></a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                        <!-- Earnings (Monthly) Card Example -->
+                        <div class="col-xl-3 col-md-6 mb-4">
+                            <div class="card border-left-success shadow h-100 py-2 bg-danger">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col mr-2">
+                                            <div class="text-md font-weight-bold text-white text-uppercase mb-1">Buku yang dipinjam</div>
+                                            <div class="h1 mb-0 font-weight-bold text-white"> <?php
+                                            $where = ['dipinjam != 0'];
+                                            $totaldipinjam = $this->ModelBuku->total('dipinjam', 
+                                            $where);
+                                            echo $totaldipinjam;?>
+                                            </div>
                                         </div>
                                         <div class="col-auto">
-                                            <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
+                                            <a href="<?= base_url('user'); ?>"><i class="fas fa-user-tag fa-3x text-success"></i></a>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
 
-                        <!-- Earnings (Monthly) Card Example -->
-                        <div class="col-xl-3 col-md-6 mb-4">
-                            <div class="card border-left-info shadow h-100 py-2">
-                                <div class="card-body">
-                                    <div class="row no-gutters align-items-center">
-                                        <div class="col mr-2">
-                                            <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Tasks
-                                            </div>
-                                            <div class="row no-gutters align-items-center">
-                                                <div class="col-auto">
-                                                    <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">50%</div>
-                                                </div>
-                                                <div class="col">
-                                                    <div class="progress progress-sm mr-2">
-                                                        <div class="progress-bar bg-info" role="progressbar"
-                                                            style="width: 50%" aria-valuenow="50" aria-valuemin="0"
-                                                            aria-valuemax="100"></div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="col-auto">
-                                            <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
 
                         <!-- Pending Requests Card Example -->
                         <div class="col-xl-3 col-md-6 mb-4">
-                            <div class="card border-left-warning shadow h-100 py-2">
+                            <div class="card border-left-warning shadow h-100 py-2 bg-success">
                                 <div class="card-body">
                                     <div class="row no-gutters align-items-center">
                                         <div class="col mr-2">
-                                            <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
-                                                Pending Requests</div>
-                                            <div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
-                                        </div>
-                                        <div class="col-auto">
-                                            <i class="fas fa-comments fa-2x text-gray-300"></i>
+                                            <div class="text-md font-weight-bold text-white text-uppercase mb-1">Buku yang dibooking</div>
+                                            <div class="h1 mb-0 font-weight-bold text-white">
+                                                <?php
+                                                $where = ['dibooking !=0'];
+                                                $totaldibooking = $this->ModelBuku->total('dibooking', $where);
+                                                echo $totaldibooking; ?>
+                                                </div>
+                                            </div>
+                                            <div class="col-auto">
+                                                <a href="<?= base_url('user'); ?>"><i class="fas fa-shopping-cart fa-3x text-danger"></i></a>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
 
                     <!-- Content Row -->
 
