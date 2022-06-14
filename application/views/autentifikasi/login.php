2818b9d5152bda8a5e3ae9d5b64ac0ac720ae8ad
FathurID <fathuriz.id@gmail.com>
2022-06-14 10:25:03 +0700

    menabahkan file pustaka-booking

--- application/views/autentifikasi/login.php
+++ application/views/autentifikasi/login.php
@@ -13,26 +13,20 @@
                                 <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
                             </div>
 
-                            <form class="user">
+                            <?= $this->session->flashdata('pesan'); ?>
+                            <form class="user" method="post" action="<?= base_url('admin/index'); ?>">
                                 <div class="form-group">
-                                    <input type="email" class="form-control form-control-user"
-                                        id="exampleInputEmail" aria-describedby="emailHelp"
-                                        placeholder="Enter Email Address...">
+                                    <input type="text" class="form-control form-control-user" value="<?= set_value
+                                    ('email'); ?>" id="email" placeholder="Masukan Alamat Email" name="email"> <?= 
+                                    form_error('email', '<small class="test-danger pl-3"', '</small>'); ?>
                                 </div>
                                 <div class="form-group">
-                                    <input type="password" class="form-control form-control-user"
-                                        id="exampleInputPassword" placeholder="Password">
+                                    <input type="password" class="form-control form-control-user" 
+                                    id="password" placeholder="Masukan Alamat Password" name="password"> <?= 
+                                    form_error('password', '<small class="test-danger pl-3"', '</small>'); ?>
                                 </div>
-                                <div class="form-group">
-                                    <div class="custom-control custom-checkbox small">
-                                        <input type="checkbox" class="custom-control-input" id="customCheck">
-                                        <label class="custom-control-label" for="customCheck">Remember
-                                            Me</label>
-                                    </div>
-                                </div>
-                                <a href="admin" class="btn btn-primary btn-user btn-block">
-                                    Login
-                                </a>
+                                <button type="submit" class="btn btn-primary btn-user btn-block"> Masuk </button>
+                            </form>
                                 
                                 <hr>
                                 <a href="index.html" class="btn btn-google btn-user btn-block">
